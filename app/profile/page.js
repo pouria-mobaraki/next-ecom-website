8101758c22419c55ba6542a6ce8754a8ec220f91
pouria-mobaraki <ss_pooria69@yahoo.com>
2025-01-26 14:01:07 +0330

    profile page and layout

---
+++ app/profile/page.js
@@ -0,0 +1,32 @@
+import { getFetch } from "@/utils/fetch"
+import { cookies } from "next/headers"
+
+
+export default async function profilePage() {
+    const token = cookies().get('token')
+   const user = await getFetch('/profile/info',{"Authorization":`Bearer ${token.value}`})
+   console.log(user,'user');
+   
+
+  return (
+    <div>
+       <div className="vh-70">
+                        <div className="row g-4">
+                            <div className="col col-md-6">
+                                <label className="form-label">نام و نام خانوادگی</label>
+                                <input type="text" className="form-control" defaultValue={user.name} />
+                            </div>
+                            <div className="col col-md-6">
+                                <label className="form-label">ایمیل</label>
+                                <input type="text" className="form-control" defaultValue={user.email} />
+                            </div>
+                            <div className="col col-md-6">
+                                <label className="form-label">شماره تلفن</label>
+                                <input type="text" disabled className="form-control" defaultValue={user.cellphone} />
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary mt-4">ویرایش</button>
+                    </div>
+    </div>
+  )
+}
